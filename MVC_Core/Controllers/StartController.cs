aee38fa47111643eae4e5e151a9690a0b39b325b
Nikola Maljenovic <mani18xi@student.ju.se>
2018-09-20 12:30:49 +0200

    Ny repository

---
+++ MVC_Core/Controllers/StartController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MVC_Core.Models;
+
+namespace MVC_Core.Controllers
+{
+    public class StartController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            var employeeList = new List<Employee>();
+
+            var employee = new Employee();
+            employee.Name = "Anders Andersson";
+            employee.PhoneNumber = "1";
+            employeeList.Add(employee);
+
+            employee = new Employee();
+            employee.Name = "Erik Andersson";
+            employee.PhoneNumber = "2";
+            employeeList.Add(employee);
+
+            employee = new Employee();
+            employee.Name = "David Andersson";
+            employee.PhoneNumber = "3";
+            employeeList.Add(employee);
+
+            employee = new Employee();
+            employee.Name = "Carl Andersson";
+            employee.PhoneNumber = "4";
+            employeeList.Add(employee);
+
+            employee = new Employee();
+            employee.Name = "Bengt Andersson";
+            employee.PhoneNumber = "5";
+            employeeList.Add(employee);
+
+            return View(employeeList);
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Blah blah blah";
+
+            return View();
+        }
+    }
+}
--- MVC_Core/Startup.cs
+++ MVC_Core/Startup.cs
@@ -24,14 +24,6 @@ namespace MVC_Core
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
@@ -42,21 +34,15 @@ namespace MVC_Core
             {
                 app.UseDeveloperExceptionPage();
             }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-                app.UseHsts();
-            }
 
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
             app.UseMvc(routes =>
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Start}/{action=Index}/{id?}");
             });
         }
     }
